27766ac33faa8a9192ef21dc7704f76ab8c7ad88
denizedizcan <70265563+denizedizcan@users.noreply.github.com>
2022-09-08 23:58:11 +0300

    middlewares

---
+++ middlewares/middlewares.go
@@ -0,0 +1,13 @@
+package middlewares
+
+import (
+	"net/http"
+)
+
+// middleware
+func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next(w, r)
+	}
+}
